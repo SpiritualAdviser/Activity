876fc6c0829c0e8ae74986233f9932a5ca638592
SpiritualAdviser <pomaxam@narod.ru>
2024-12-11 12:35:12 +0300

    add permissionCameraStatus

--- app/src/main/java/otus/gpb/homework/activities/EditProfileActivity.kt
+++ app/src/main/java/otus/gpb/homework/activities/EditProfileActivity.kt
@@ -1,20 +1,44 @@
 package otus.gpb.homework.activities
 
+import android.Manifest
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class EditProfileActivity : AppCompatActivity() {
 
     private lateinit var imageView: ImageView
 
+    var permissionCameraStatus = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { granted ->
+        when {
+            granted -> {
+                imageView.setImageResource(R.drawable.cat)
+            }
+
+            else -> {
+
+            }
+
+
+        }
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
         imageView = findViewById(R.id.imageview_photo)
+
+        imageView.setOnClickListener {
+            showAlertDialog()
+        }
 
         findViewById<Toolbar>(R.id.toolbar).apply {
             inflateMenu(R.menu.menu)
@@ -24,10 +48,28 @@ class EditProfileActivity : AppCompatActivity() {
                         openSenderApp()
                         true
                     }
+
                     else -> false
                 }
             }
         }
+    }
+
+    private fun showAlertDialog() {
+        val choosingActions = arrayOf("Сделать фото", "Выбрать фото")
+        MaterialAlertDialogBuilder(this).apply {
+            setTitle("Take a picture")
+            setSingleChoiceItems(choosingActions, -1) { dialog, item ->
+                when (item) {
+                    0 -> requestCameraPermission()
+                }
+            }.show()
+        }
+    }
+
+    fun requestCameraPermission() {
+        permissionCameraStatus.launch(Manifest.permission.CAMERA)
+        println()
     }
 
     /**
